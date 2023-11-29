3aeb84099c3785be13d3e3cb7e6d40b8ef2521cc
TheisKidi <theis.k@hotmail.com>
2023-11-29 11:52:01 +0100

    UnitTest

---
+++ SenseMaxTests/ProfileTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseMax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenseMax.Tests
+{
+    [TestClass()]
+    public class ProfileTests
+    {
+        [TestMethod()]
+        public void ProfileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ProfileTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
