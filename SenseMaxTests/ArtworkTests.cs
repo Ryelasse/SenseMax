2119c17897391f0c698e5326f55fd2c94527714e
boeger01 <matias.boeger@gmail.com>
2023-11-29 12:10:39 +0100

    art test

---
+++ SenseMaxTests/ArtworkTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseMax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenseMax.Tests
+{
+    [TestClass()]
+    public class ArtworkTests
+    {
+        [TestMethod()]
+        public void ArtworkTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateTempTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateHumidityTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
