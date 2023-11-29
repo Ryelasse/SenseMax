8b649c3be2339a02e80b11dd6425dd04898e6d46
Nicklas Peters Pedersen <nick7845@edu.zealand.dk>
2023-11-29 13:12:03 +0100

    Secret

---
+++ SenseRepositoryDB/Secret.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenseRepositoryDB
+{
+    public class Secret
+    {
+        public static string GetConnectionString
+        {
+            get
+            {
+                return "Data Source=mssql9.unoeuro.com;User ID=lntech_dk;Password=trEG49cf2A3FBRxp5ygz;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            }
+        }
+    }
+}
